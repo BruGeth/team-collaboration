02258f5acc599b3c083c0a9893b3707b22a8d2f3
Bruno <150201562+BruGeth@users.noreply.github.com>
2025-05-01 21:08:25 -0500

    Revert "feat(frontend): sigo probando mis commits"
    This reverts commit 9b95ee75f3b27979e6d82051a023829a83b43ca6.

--- test.java
+++ test.java
@@ -8,5 +8,3 @@ system.out.println("adios mundo");
 system.out.println("hola estoy probando mis commits en git");
 
 system.out.println("adios estoy probando mis commits en git");
-
-system.out.println("hola estoy probando mis commits en git desde la misma rama");
